89469e2d6871fa874fd53ee2c3f85bea59e4c9be
Thedduro <lsw2207@gmail.com>
2026-04-07 23:38:18 +0900

    Initial Commit

---
+++ snapfyTests/snapfyTests.swift
@@ -0,0 +1,18 @@
+//
+//  snapfyTests.swift
+//  snapfyTests
+//
+//  Created by Thedduro on 4/7/26.
+//
+
+import Testing
+
+struct snapfyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
