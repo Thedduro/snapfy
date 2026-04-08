66a85dca8fc20e5be98ca9d20f08e01d38b93d61
Thedduro <lsw2207@gmail.com>
2026-04-08 15:19:21 +0900

    feat: 미디어 엔트리 확장

--- snapfy/Core/MediaModels.swift
+++ snapfy/Core/MediaModels.swift
@@ -5,18 +5,27 @@ import SwiftData
 final class MediaEntry {
     @Attribute(.unique) var id: UUID
     var date: Date
-    var imageData: Data
+    var imageData: Data?
+    var videoData: Data?
+    var thumbnailData: Data
+    var mediaType: String
     var createdAt: Date
 
     init(
         id: UUID = UUID(),
         date: Date,
-        imageData: Data,
+        imageData: Data? = nil,
+        videoData: Data? = nil,
+        thumbnailData: Data,
+        mediaType: String,
         createdAt: Date = .now
     ) {
         self.id = id
         self.date = Calendar.current.startOfDay(for: date)
         self.imageData = imageData
+        self.videoData = videoData
+        self.thumbnailData = thumbnailData
+        self.mediaType = mediaType
         self.createdAt = createdAt
     }
 }
